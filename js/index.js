9d1d4ef6d17bc4966c496dcf0d94ff00e32f62eb
Jessie <isai@protonmail.com>
2017-02-09 02:15:56 +0100

    Changed style to incorperate the materialize css library & changed how tweet btn works.
    Simple material design was implemented with the materialize css library. Tweet was changed to just be an anchor with the appropriate link. This was done to avoid having to reload the twitter btn widgtet everytime the quote changed, since there was a short delay were the anchor was exposed.

--- js/index.js
+++ js/index.js
@@ -1,120 +1,131 @@
-var authorTextLength;
+
 var quoteBuffer = [];
-var usedQuotes = [];
-var loadClasses = ("glyphicon glyphicon-refresh glyphicon-refresh-animate");
-var isWaitingForResponse = false;
-var bufferSize = 20;
 
 $(document).ready(function() {
+
+  var isWaitingForResponse = false;
+  var bufferSize = 20;
+  var authorTextLength;
 
     authorTextLength = $("#author-text").html().length;
 
     updateQuote();
-    $("#new-quote-btn").on("click", function() {
-        $("#new-quote-btn").blur();
-        if (!$("#load-icon").hasClass("glyphicon")) {
-            updateQuote();
-        }
-    });
+    updateOnBtnClicked();
+    updateOnSpacebarPress();
+    preventTweetBtnFromKeepingFocus();
 
-});
-
-$(document).keypress(function(key){
-  if(key.which == 32){
-    if (!$("#load-icon").hasClass("glyphicon")) {
-        updateQuote();
-    }
-  }
-
-});
-
-function updateQuote() {
-
-    toggleLoadIcon(true);
-        console.log("tweet: "+$(".twitter-share-button").html());
-    if (quoteBuffer.length < 1) {
-        refillBuffer(true);
-    } else {
-        usedQuotes.push(quoteBuffer[quoteBuffer.length - 1]);
-        updateTweet(quoteBuffer[0]);
-        $("#quote-text").html(quoteBuffer.shift());
-        toggleLoadIcon(false);
-
-        if(quoteBuffer.length < bufferSize){
-          refillBuffer(false);
-        }
-    }
+    function updateQuote() {
 
 
-};
-
-function refillBuffer(shouldUpdateQuote) {
-
-    if (usedQuotes.length > 100) {
-        usedQuotes = [];
-    }
-
-    if(!isWaitingForResponse){
-      isWaitingForResponse = true;
-    QuoteApi().getRandomTitleArray(function(quote) {
-
-        isWaitingForResponse = false;
-        if (shouldUpdateQuote || $("#load-icon").hasClass("glyphicon")) {
-            $("#quote-text").html(quote);
-            updateTweet(quote);
-            $("#tweet-btn").attr("data-text", "this is a test");
-            shouldUpdateQuote = false;
-            toggleLoadIcon(false);
+        if (quoteBuffer.length < 1) {
+            toggleLoadIcon(true);
+            refillBuffer(true);
         } else {
 
-          quoteBuffer.push(quote);
-          console.log(quoteBuffer.length);
+            updateTweet(quoteBuffer[0]);
+            $("#quote-text").html(quoteBuffer.shift());
+
+
+            if (quoteBuffer.length < bufferSize) {
+                refillBuffer(false);
+            }
         }
 
-        if(quoteBuffer.length < bufferSize){
-          refillBuffer(false);
+
+    };
+
+    function updateOnBtnClicked(){
+      $("#new-quote-btn").click( function() {
+          $("#new-quote-btn").blur();
+          if (!$("#load-icon").hasClass("glyphicon")) {
+              updateQuote();
+          }
+      });
+    };
+
+    function updateOnSpacebarPress(){
+
+      $(document).keypress(function(key) {
+        if (key.which == 32) {
+            if (!$("#load-icon").hasClass("glyphicon")) {
+                updateQuote();
+            }
         }
-
-
-    }, function(){
-      isWaitingForResponse = false;
-      refillBuffer(false);
 
     });
-  }
+    };
+
+    function preventTweetBtnFromKeepingFocus(){
+
+          $("#tweet-anchor").focus( function(){
+            console.log("tweet clicked");
+            $("#tweet-anchor").blur();
+          });
+    }
+
+    function refillBuffer(shouldUpdateQuote) {
+
+      console.log($("#load-animation-div").hasClass("hide"));
+
+
+        if (!isWaitingForResponse) {
+
+            isWaitingForResponse = true;
+            QuoteApi().getRandomQuote(function(quote) {
+
+                isWaitingForResponse = false;
+                if (shouldUpdateQuote || $("#load-animation-div").hasClass("hide") == false) {
+                    toggleLoadIcon(false);
+                    $("#quote-text").html(quote);
+                    updateTweet(quote);
+                    shouldUpdateQuote = false;
+
+                } else {
+                    toggleLoadIcon(false);
+                    quoteBuffer.push(quote);
+                    console.log(quoteBuffer.length);
+                }
+
+                if (quoteBuffer.length < bufferSize) {
+                    refillBuffer(false);
+                }
+
+
+            }, function() {
+                isWaitingForResponse = false;
+                refillBuffer(false);
+
+            });
+        }
 
 
 
-};
+    };
 
 
-function updateTweet(quote){
-  var tweetBtn = document.querySelector(".twitter-share-button");
-  var anchor = document.createElement("a");
+    function updateTweet(quote) {
 
-  anchor.setAttribute("href", "https://twitter.com/share");
-  anchor.setAttribute("class", "twitter-share-button");
-  anchor.setAttribute("data-text", quote);
-  anchor.setAttribute("id", "twitter");
-  anchor.setAttribute("data-show-count", "true");
-  anchor.innerHTML = "Tweet";
-  tweetBtn.parentNode.replaceChild(anchor, tweetBtn);
-  twttr.widgets.load();
-  var html = $("#tweet-btn").html();
-
-  console.log("html: "+html);
-}
-
-
-
-
-function toggleLoadIcon(showIcon) {
-    var loadClasses = ("glyphicon glyphicon-refresh glyphicon-refresh-animate");
-    if (showIcon) {
-        $("#load-icon").addClass(loadClasses);
-    } else {
-        $("#load-icon").removeClass(loadClasses);
+        var tweetBtn = document.querySelector(".twitter-share-button");
+        var quoteFiltered = quote.replace("—", "-");
+        console.log("filtered: "+quoteFiltered+"  "+quote);
+        $("#tweet-anchor").attr("href", "http://twitter.com/share?text="+String(quoteFiltered)+" -  Donald J Trump");
+        console.log("update twitter btn");
 
     }
 
-};
+
+
+
+    function toggleLoadIcon(showIcon) {
+        console.log("showIcon: "+showIcon);
+        if (showIcon) {
+            $("#load-animation-div").removeClass("hide");
+        } else {
+            $("#load-animation-div").addClass("hide");
+
+        }
+
+    };
+
+
+});
